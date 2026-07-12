7b8c6a73538b3b62d303068a8e0fdb32bb6ed379
pavlojs <pavlojs@gmail.com>
2026-07-12 17:07:33 +0200

    refactor(ui): move hiltViewModel imports to the new package

--- app/src/main/java/net/whiteravens/dietapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/net/whiteravens/dietapp/ui/navigation/AppNavigation.kt
@@ -20,7 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
--- app/src/main/java/net/whiteravens/dietapp/ui/screens/recipes/RecipesScreen.kt
+++ app/src/main/java/net/whiteravens/dietapp/ui/screens/recipes/RecipesScreen.kt
@@ -30,7 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.distinctUntilChanged
 import net.whiteravens.dietapp.R
